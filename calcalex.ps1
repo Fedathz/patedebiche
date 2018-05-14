ee61abba2db4eaf625da95db587aa0fb46d9f5cb
Fedathz <fedathz@hotmail.fr>
2018-05-14 15:42:09 +0200

    Ok

---
+++ calcalex.ps1
@@ -0,0 +1,55 @@
+﻿Write-Host `t`t "Ceci est une calculatrice en bois" -backgroundColor black 
+
+[int]$nb1 = Read-Host "Premiere valeur"
+$op = Read-Host "Opérateur - + / * %"
+[int]$nb2= Read-Host "Deuxieme valeur"
+<#
+if ($op -eq "+")
+{
+    $res = $nb1 + $nb2
+    Write-Host $res
+    exit 0
+}
+
+if ($op -eq "-")
+{
+    $res = $nb1 - $nb2
+    Write-Host $res
+    exit 0
+}
+
+if ($op -eq "*")
+{
+    $res = $nb1 * $nb2
+    Write-Host $res
+    exit 0
+}
+
+if ($op -eq "/")
+{
+    $res = $nb1 / $nb2
+    Write-Host $res
+    exit 0
+}
+#>
+#faire avec un switch
+switch ($op)
+{
+    "+"
+    {
+    $res = $nb1 + $nb2
+    }
+    "-"
+    {
+      $res = $nb1 - $nb2
+    }
+    "/"
+    {
+    $res = $nb1 / $nb2
+    }
+       "*"
+    {
+    $res = $nb1 * $nb2
+    }
+}
+Write-Host $res
